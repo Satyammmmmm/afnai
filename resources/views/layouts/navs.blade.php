87536d3778488cf9644b51be4e0caa9399949bf3
OhM <64514042+omthapa779@users.noreply.github.com>
2025-03-04 18:06:02 +0545

    NAVBAR STATIC

--- resources/views/layouts/navs.blade.php
+++ resources/views/layouts/navs.blade.php
@@ -8,6 +8,19 @@
     <link rel="stylesheet" href="{{ asset('./resources/css/util.css') }}">
 </head>
 <body>
-    @yield('content')
+    <div class="main_container w_100">
+        <nav class="navbar w_100 h_10vh padding_s15 flex justify_sb align_c">
+            <h1 class="primary_font">SASTA PATHAO</h1>
+
+            <div class="nav_links flex gap_s">
+                <a href="" class="primary_font primary_color"><h3 class="font_w400">Services</h3></a>
+                <a href="" class="primary_font primary_color"><h3 class="font_w400">Become a Rider</h3></a>
+                <a href="" class="primary_font primary_color"><h3 class="font_w400">Contact</h3></a>
+            </div>
+        </nav>
+        <main class="main w_100">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
