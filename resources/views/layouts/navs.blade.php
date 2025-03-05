a6e22ecc8d7512206c41c2a89e6f7a16496d5d84
Satyammmmmm <gusionyt3903@gmail.com>
2025-03-05 14:43:01 +0545

    admin edited

--- resources/views/layouts/navs.blade.php
+++ resources/views/layouts/navs.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>yield ('title', '')</title>
+    <title >@yield('title','')</title>
     <link rel="stylesheet" href="{{ asset('./resources/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('./resources/css/util.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css">
